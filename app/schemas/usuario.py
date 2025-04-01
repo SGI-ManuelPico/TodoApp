8f297a7b833ac10a914ba5a420f8dcc4874a45b9
SGI-ManuelPico <desarrollo.software3@sgiltda.com>
2025-04-01 12:05:22 -0500

    Update usuario.py

--- app/schemas/usuario.py
+++ app/schemas/usuario.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, EmailStr, Field, field_validator
 from typing import List, Optional
-from app.utils.validaciones import validar_contraseña  # Use absolute import
+from app.utils.validaciones import validar_contraseña
 
 class UsuarioBase(BaseModel):
     nombre: str = Field(..., min_length=2, max_length=50)
